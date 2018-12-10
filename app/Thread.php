644f5144cde5977ac2476fb10b22ef8768d54699
Eugene Yurkov <y.yurkov@svitla.com>
2018-12-10 15:57:54 +0200

    Add relations.

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function threadResponses()
+    {
+        return $this->hasMany('App\ThreadResponse');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/ThreadResponse.php
+++ app/ThreadResponse.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ThreadResponse extends Model
 {
-    //
+    public function thread()
+    {
+        return $this->belongsTo('App\Thread');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
